09052ff3d5aed497f7292f6ca677b8e5e47f8eb1
sandygp786 <sandygp786@gmail.com>
2025-04-19 02:05:09 +0530

    2 files added
    Contains Assignments for Control structure Cases

---
+++ Assignments3.py
@@ -0,0 +1,6 @@
+Num=int(input("Enter a Number :"))
+rem1=Num % 2
+if (rem1==0):
+    print(Num,"is an even number.")
+else:
+    print(Num,"is an odd number.")
---
+++ Assignments4.py
@@ -0,0 +1,5 @@
+Num=range(1,51)
+sum=0
+for i in Num:
+    sum=sum + i
+print("The sum of numbers from 1 to 50 is :",sum)
